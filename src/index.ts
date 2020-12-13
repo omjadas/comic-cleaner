b398398db04462f977894d369f95caeba6fc910e
omjadas <omja.das@gmail.com>
2020-12-13 20:32:59 +1100

    chore: use yargs

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,59 @@
 #!/usr/bin/env node
 
+import { promises as fs } from "fs";
 import Seven from "node-7z";
 import path from "path";
-import { promises as fs } from "fs";
+import yargs from "yargs";
 
-async function main() {
-    const file = path.join(process.cwd(), process.argv[2]);
-
-    const extractStream = Seven.extractFull(
-        file,
-        file + ".tmp",
-        {
-            recursive: true,
-        }
-    );
-
-    extractStream.on("end", async () => {
-
-        const addStream = Seven.add(
-            file.split('.').slice(0, -1).join('.') + ".cbz",
-            file + ".tmp/*",
-            {
-                archiveType: "zip",
-                recursive: true,
-            }
-        );
-
-        addStream.on("end", async () => {
-            await Promise.all([
-                fs.rm(file),
-                fs.rm(file + ".tmp", {
+yargs
+    .command("$0 <files..>", "clean a comic file", yargs => {
+        return yargs
+            .positional("files", {
+                type: "string",
+                array: true,
+                demandOption: "true",
+                coerce: (files: string[]) => {
+                    return files.map(file => {
+                        return path.join(process.cwd(), file);
+                    });
+                },
+            })
+            .option("delete", {
+                type: "string",
+                array: true,
+                alias: "d",
+            });
+    }, argv => {
+        argv.files.forEach(file => {
+            const extractStream = Seven.extractFull(
+                file,
+                file + ".tmp",
+                {
                     recursive: true,
-                    force: true,
-                }),
-            ]);
-        });
-    });
-}
+                }
+            );
 
-main();
+            extractStream.on("end", async () => {
+                const addStream = Seven.add(
+                    file.split('.').slice(0, -1).join('.') + ".cbz",
+                    file + ".tmp/*",
+                    {
+                        archiveType: "zip",
+                        recursive: true,
+                    }
+                );
+
+                addStream.on("end", async () => {
+                    await Promise.all([
+                        argv.delete ? fs.rm(file) : Promise.resolve(),
+                        fs.rm(file + ".tmp", {
+                            recursive: true,
+                            force: true,
+                        }),
+                    ]);
+                });
+            });
+        });
+    })
+    .completion()
+    .argv;
