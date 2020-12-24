a80a33aa664ebafd7c026a819b79d74baed24161
omjadas <omja.das@gmail.com>
2020-12-24 12:22:11 +1100

    feat: display progress bar

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+import { SingleBar } from "cli-progress";
 import { promises as fs } from "fs";
 import glob from "glob";
 import Seven from "node-7z";
 import path from "path";
+import restoreCursor from "restore-cursor";
 import util from "util";
 import yargs from "yargs";
 
@@ -34,6 +36,15 @@ yargs
                 default: [] as string[],
             });
     }, argv => {
+        restoreCursor();
+
+        const singleBar = new SingleBar({
+            hideCursor: true,
+            stopOnComplete: true,
+        });
+
+        singleBar.start(argv.files.length, 0);
+
         argv.files.forEach(file => {
             const dir = file + ".tmp.d";
             const newFile = file.split('.').slice(0, -1).join('.') + ".cbz";
@@ -72,15 +83,16 @@ yargs
                 );
 
                 addStream.on("end", () => {
-                    if (argv.delete) {
-                        fs.unlink(file);
-                    }
-
-                    fs.rename(tmpFile, newFile);
-
-                    fs.rmdir(dir, {
-                        recursive: true,
-                    });
+                    Promise.all([
+                        argv.delete ? fs.unlink(file) : Promise.resolve(),
+                        fs.rename(tmpFile, newFile),
+                        fs.rmdir(dir, {
+                            recursive: true,
+                        }),
+                    ])
+                        .then(() => {
+                            singleBar.increment();
+                        });
                 });
             });
         });
